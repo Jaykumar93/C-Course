411fce5a0bfb08080ca42e5df7fddaa2d6a94c2a
Jaykumar Pokar <jaykumar.pokar@coditas.com>
2024-03-11 09:40:13 +0530

    Tuples : Completed

---
+++ Section/Section 22 b/CovarianceExample/CovarianceExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianceExample
+{
+    class LivingThing
+    {
+        public int NumberOfLegs {  get; set; } 
+    }
+
+    class Parrot: LivingThing
+    {
+
+    }
+
+    class Dog : LivingThing
+    {
+
+    }
+    
+    interface IMover<out T>
+    {
+        T Move();
+    }
+    class  Mover<T>:IMover<T>
+    {
+        public T thing { get; set; }
+        public T Move()
+        {
+            return thing;
+        }
+    }
+    class Program
+    {
+        static void Main( )
+        {
+            LivingThing livingthing = new Parrot(); // this is normal behaviour of the class inhiretance
+            Parrot parrot = new Parrot() { NumberOfLegs = 2};
+
+            IMover<LivingThing> mover = new Mover<Parrot>();
+            Console.WriteLine("Moving with" + mover.Move().NumberOfLegs + "legs");
+
+        }
+    }
+}
---
+++ Section/Section 22 b/IComperableCollection/IComperableCollection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComperableCollection
+{
+    class Employee:IComparable
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Job {  get; set; }
+
+        public int CompareTo(object obj)
+        {
+           // Console.WriteLine(this.EmpId + " || " + ((Employee)obj).EmpId);
+           Employee otherEmp = (Employee)obj;
+
+            return this.EmpName.CompareTo(otherEmp.EmpName);
+        }
+    }
+    class Program
+    {
+        
+        static void Main()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(){EmpId=104,EmpName="Jay",Job="Engineer"},
+                new Employee(){EmpId=105,EmpName="Kartik",Job="Designer"},
+                new Employee(){EmpId=101,EmpName="Avinash",Job="Scientist"},
+                new Employee(){EmpId=103,EmpName="Keval",Job="Sales"}
+            };
+            employees.Sort();
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item.EmpId + " || " + item.EmpName + " || " + item.Job);
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}

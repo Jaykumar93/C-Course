53baceb124ad28d55ba18c39b20fbae7d724926b
Jaykumar Pokar <jaykumar.pokar@coditas.com>
2024-03-21 14:40:23 +0530

    ConcurrentDictionary example

---
+++ Section/Examples/ConcurrentDictionary2/ConcurrentDictionary2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class CustomDictionary<TKey, TValue>
+{
+    private Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+    public void Write(object obj)
+    {
+        var parameters = (Tuple<TKey, TValue>)obj;
+        dictionary[parameters.Item1] = parameters.Item2;
+        Console.WriteLine($"Added: Key = {parameters.Item1}, Value = {parameters.Item2}");
+        
+    }
+
+    public void Read(object obj)
+    {
+        var key = (TKey)obj;
+        TValue value = dictionary[key];
+        Console.WriteLine($"Read: Key = {key}, Value = {value}");
+        
+    }
+
+    public void Remove(object obj)
+    {
+        var key = (TKey)obj;
+        dictionary[key] =default(TValue);
+        Console.WriteLine($"Removed: Key = {key}"); 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomDictionary<int, string> customDictionary = new CustomDictionary<int, string>();
+
+        // Create three threads using ParameterizedThreadStart delegates
+        Thread thread1 = new Thread(new ParameterizedThreadStart(customDictionary.Write));
+        Thread thread2 = new Thread(new ParameterizedThreadStart(customDictionary.Read));
+        Thread thread3 = new Thread(new ParameterizedThreadStart(customDictionary.Remove));
+
+        // Start the threads
+        thread1.Start(Tuple.Create(1, "One"));
+        thread2.Start(1);
+        thread1.Start(Tuple.Create(2, "Two"));
+        thread2.Start(2);
+        thread3.Start(3);
+
+       
+
+        // Wait for all threads to complete
+        thread1.Join();
+        thread2.Join();
+        thread3.Join();
+    }
+}
---
+++ Section/Examples/ConcurrentDictonary/ConcurrentDictonary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static ConcurrentDictionary<int, string> keyValuePairs = new ConcurrentDictionary<int, string>();
+
+    static void Main(string[] args)
+    {   
+
+        Thread thread1 = new Thread(AddToDictionary);
+        Thread thread2 = new Thread(AddToDictionary);
+        thread1.Start();
+        thread1.Name = "THREAD NO 1";
+        thread2.Start();
+        thread2.Name = "THREAD NO 2";
+
+    }
+
+    static void AddToDictionary()
+    {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) 
+        {
+            int key = random.Next(100);
+            keyValuePairs.TryAdd(key, Thread.CurrentThread.Name); 
+            Console.WriteLine($"Thread {Thread.CurrentThread.Name} added to dictionary with key {key}");
+            Thread.Sleep(10); 
+        }
+    }
+}
